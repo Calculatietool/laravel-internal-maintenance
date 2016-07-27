e6c837f5390c6c48d5c2b55ef7066e9b5da131ca
yorickdewid <yorick17@outlook.com>
2016-07-27 23:06:15 +0200

    Project entity

---
+++ src/Entities/ProjectList.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace CalculatieTool\IntMaint\Entities;
+
+use CalculatieTool\IntMaint\Contracts\EntityInterface;
+
+class ProjectList implements EntityInterface
+{
+    /**
+     * The entity uri.
+     *
+     * @var \GuzzleHttp\Client
+     */
+    protected $uri = '/oauth2/rest/internal/project_all';
+
+    /**
+     * Retrieve entity uri.
+     *
+     * @return string
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * Loop over each retrieved user.
+     *
+     * @param  Clojure  $callback
+     */
+    public function handle($response, $callback)
+    {
+        foreach ($response as $project) {
+            $callback($project);
+        }
+    }
+}
